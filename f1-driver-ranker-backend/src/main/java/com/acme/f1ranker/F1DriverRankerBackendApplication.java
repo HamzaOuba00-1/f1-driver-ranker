2df3b50eb635bfc0f5997f2e7c52b922187165b1
HamzaOuba00-1 <hamza.ouba.2004@gmail.com>
2026-02-07 22:43:18 +0200

    chore: bootstrap monorepo (spring boot + angular)

---
+++ f1-driver-ranker-backend/src/main/java/com/acme/f1ranker/F1DriverRankerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.f1ranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1DriverRankerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1DriverRankerBackendApplication.class, args);
+	}
+
+}
---
+++ f1-driver-ranker-backend/src/test/java/com/acme/f1ranker/F1DriverRankerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.f1ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1DriverRankerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
